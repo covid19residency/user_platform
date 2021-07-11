47aea93c4fc084ae556f4a5d8fc994c4aec82f8c
unknown <cindycindy1998@qq.com>
2021-07-12 01:32:40 +0800

    test3

--- run.R
+++ run.R
@@ -2,8 +2,9 @@ library(shiny)
 # Gets the port # from Heroku
 port <- Sys.getenv('PORT')
 library(reticulate)
-py_config()
+reticulate::install_miniconda()
 py_available()
+py_config()
 # Runs the app in the working directory. By default this will search for an `app.R` file or 
 # a ui.R + server.R file combination
 # The host of 0.0.0.0 indicates the app runs at the web server's ip address
